77da724424f5538e72788b8cc1f553d75a324753
Gabriel Cardoso <gabriellcardoso@gmail.com>
2015-05-07 20:51:02 -0300

    Adding class to handle frame writing and reading just creating one time the byte 2d array

---
+++ src/br/edu/ufpel/inf/motionestimation/Frame.java
@@ -0,0 +1,31 @@
+package br.edu.ufpel.inf.motionestimation;
+
+public class Frame {
+
+	byte[][] frame;
+	int width;
+	int height;
+	
+	public Frame(int width, int height) {
+		this.width = width;
+		this.height = height;
+		createFrame();
+	}
+	
+	private void createFrame() {
+		frame = new byte[height][width];
+	}
+	
+	public byte[][] getImage() {
+		return frame;
+	}
+	
+	public int getWidth() {
+		return width;
+	}
+	
+	public int getHeight() {
+		return height;
+	}
+	
+}
