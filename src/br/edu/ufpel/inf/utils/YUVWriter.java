b127e1dfb5b9b74bcfd6996ff6b6d32ce7494724
Gabriel Cardoso <gabriellcardoso@gmail.com>
2015-05-07 21:52:03 -0300

    Adding YUVWriter class to handle writing yuv files

---
+++ src/br/edu/ufpel/inf/utils/YUVWriter.java
@@ -0,0 +1,54 @@
+package br.edu.ufpel.inf.utils;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class YUVWriter {
+
+    private FileOutputStream outputStream;
+
+    public YUVWriter(String fileName) {
+        openOutputStream(fileName);
+    }
+    
+    public void writeFrameOnFile(Frame frame) {
+    	byte[][] image = frame.getImage();
+        int index;
+
+        for (index = 0; index < frame.getHeight(); index++) {
+            writeLine(image[index]);
+        }
+    }
+    
+    private void writeLine(byte[] bytes) {
+    	try {
+    		outputStream.write(bytes);
+    	}
+    	catch (IOException error) {
+    		System.err.println(error.getMessage());
+    	}
+    }
+
+    private void openOutputStream(String fileName) {
+    	try {
+            outputStream = new FileOutputStream(fileName);
+        }
+        catch (IOException error) {
+            System.err.println(error.getMessage() + " : " + fileName);
+        }
+    }
+
+    private void closeOutputStream() {
+        try {
+            outputStream.flush();
+            outputStream.close();
+        } catch (IOException error) {
+            System.err.println("Error closing " + outputStream + " : " + error.getMessage());
+        }
+    }
+    
+    protected void finalize() {
+    	closeOutputStream();
+    }
+
+}
