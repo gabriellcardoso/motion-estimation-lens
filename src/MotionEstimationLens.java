86b8bbf20d398a255336d47d6dd1cf6a54404fe2
Gabriel Cardoso <gabriellcardoso@gmail.com>
2015-05-07 21:55:44 -0300

    Main class now write the luminance part of the yuv file in another file based on args from the command line

--- src/MotionEstimationLens.java
+++ src/MotionEstimationLens.java
@@ -1,3 +1,7 @@
+import br.edu.ufpel.inf.utils.Frame;
+import br.edu.ufpel.inf.utils.YUVReader;
+import br.edu.ufpel.inf.utils.YUVWriter;
+
 
 public class MotionEstimationLens {
 
@@ -5,8 +9,19 @@ public class MotionEstimationLens {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String fileName = args[0];
+		int width = Integer.parseInt(args[1]);
+		int height = Integer.parseInt(args[2]);
+		int sampling = Integer.parseInt(args[3]);
+		
+		YUVReader reader = new YUVReader(fileName, width, height, sampling);
+		YUVWriter writer = new YUVWriter(fileName + ".out");
+		
+		Frame actualFrame = new Frame(width, height);
+		
+		for (int i = 0; i < 300; i++) {
+			reader.setFrameWithImage(actualFrame, i);
+			writer.writeFrameOnFile(actualFrame);
+		}
 	}
-
 }
