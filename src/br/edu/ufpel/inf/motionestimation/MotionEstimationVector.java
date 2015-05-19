7bb9a4632cf68092e75ac6448b2170a009854ad7
Gabriel Cardoso <gabriellcardoso@gmail.com>
2015-05-19 02:45:31 -0300

    Implementing a motion estimation vector class

--- src/br/edu/ufpel/inf/motionestimation/MotionEstimationVector.java
+++ src/br/edu/ufpel/inf/motionestimation/MotionEstimationVector.java
@@ -1,5 +1,52 @@
 package br.edu.ufpel.inf.motionestimation;
 
+import br.edu.ufpel.inf.utils.CodingBlock;
+import br.edu.ufpel.inf.utils.Position;
+
 public class MotionEstimationVector {
 
+	private CodingBlock codingBlock;
+	private Position position;
+	private int criteriaResult;
+	
+	public MotionEstimationVector(CodingBlock block) {
+		position = new Position();
+		criteriaResult = Integer.MAX_VALUE;
+		
+		createCodingBlock(block);
+	}
+	
+	private void createCodingBlock(CodingBlock block) {
+		try {
+			codingBlock = (CodingBlock) block.clone();
+		}
+		catch (CloneNotSupportedException exception) {
+			System.err.println(exception.getMessage());
+		}
+	}
+	
+	public CodingBlock getCodingBlock() {
+		return codingBlock;
+	}
+	
+	public Position getPosition() {
+		return position;
+	}
+	
+	public int getCriteriaResult() {
+		return criteriaResult;
+	}
+	
+	public void setPosition(int x, int y) {
+		position.setPosition(x, y);
+	}
+	
+	public void setCriteriaResult(int result) {
+		criteriaResult = result;
+	}
+	
+	
+	
+	
+	
 }
