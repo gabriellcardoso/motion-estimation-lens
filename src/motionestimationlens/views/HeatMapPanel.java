d031ff710e4a39e892500070018ee01674932e74
Gabriel Cardoso <gabriel.cardoso@way2.com.br>
2015-06-09 12:14:21 -0300

    Removing unnecessary code from heat map panel component

--- src/motionestimationlens/views/HeatMapPanel.java
+++ src/motionestimationlens/views/HeatMapPanel.java
@@ -20,47 +20,8 @@ public class HeatMapPanel extends HeatMap {
 	}
 	
 	public void updateData(double[][] data) {
-		Dimension size = new Dimension(data[0].length, data.length);
-
-		setMinimumSize(size);
-		setPreferredSize(size);
-		setMaximumSize(size);
-		
 		updateData(data, USE_GRAPHICS_Y_AXIS);
 	}
-	
-	@Override
-	protected void updateDataColors()
-    {
-        //We need to find the range of the data values,
-        // in order to assign proper colors.
-        double largest = Double.MIN_VALUE;
-        double smallest = Double.MAX_VALUE;
-        for (int x = 0; x < data.length; x++)
-        {
-            for (int y = 0; y < data[0].length; y++)
-            {
-                largest = Math.max(data[x][y], largest);
-                smallest = Math.min(data[x][y], smallest);
-            }
-        }
-        double range = largest - smallest;
-
-        // dataColorIndices is the same size as the data array
-        // It stores an int index into the color array
-        dataColorIndices = new int[data.length][data[0].length];    
-
-        //assign a Color to each data point
-        for (int x = 0; x < data.length; x++)
-        {
-            for (int y = 0; y < data[0].length; y++)
-            {
-                double norm = (data[x][y] - smallest) / range; // 0 < norm < 1
-                int colorIndex = (int) Math.floor(norm * (colors.length - 1));
-                dataColorIndices[x][y] = colorIndex;
-            }
-        }
-    }
 	
 	public void setBestBlock(MotionEstimationVector vector) {
 		setBlock(
